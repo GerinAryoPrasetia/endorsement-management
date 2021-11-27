e1e87e2b90494bdf0a3c2d4c5679e021f2fb45f2
gerinaryo <mbpro-2015@gerins-macbook.local>
2021-11-27 11:45:38 +0700

    add data layer for creator registration

--- features/creator/data/record.go
+++ features/creator/data/record.go
@@ -11,16 +11,86 @@ type Creator struct {
 	Name string
 	Location string
 	Age int
+	Gender string
+	Email string
+	Password string
+	Bio string
+	Category []Category `gorm:"many2many:creator_category;"`
+	SocialMedia []SocialMedia
+}
+
+type Category struct {
+	gorm.Model
+	CategoryName string
 }
 
 type SocialMedia struct {
 	gorm.Model
+	CreatorID int
 	SocialMediaName string
 	Url string
 	Followers int
 	VerifiedStatus bool
+	RatePrice int
+
 }
 
+type FeaturedContent struct {
+	gorm.Model
+	Url string
+}
+
+func toCategoryRecords(category []creator.CategoryCore) []Category {
+	convertedCategory := []Category{}
+	for _, cat := range category {
+		convertedCategory = append(convertedCategory, Category{
+			CategoryName: cat.CategoryName,
+		})
+	}
+
+	return convertedCategory
+}
+
+func toSocialMediaRecords(socmed []creator.SocialMediaCore) []SocialMedia {
+	convertedSocmed := []SocialMedia{}
+	for _, soc := range socmed {
+		convertedSocmed = append(convertedSocmed, SocialMedia{
+			SocialMediaName: soc.Name,
+			Url: soc.Url,
+			Followers: soc.Followers,
+			VerifiedStatus: soc.VerifiedStatus,
+			CreatorID: soc.CreatorID,
+			RatePrice: soc.RatePrice,
+		})
+	}
+	return convertedSocmed
+}
+
+func toFeaturedRecords(cont []creator.FeaturedContent) []FeaturedContent {
+	convertedContent := []FeaturedContent{}
+	for _,con := range cont {
+		convertedContent = append(convertedContent, FeaturedContent{
+			Url: con.Url,
+		})
+	}
+	return convertedContent
+}
+
+//register creator
+func toCreatorRecord(creator creator.Core) Creator {
+	return Creator{
+		Name: creator.Name,
+		Location: creator.Location,
+		Age: creator.Age,
+		Gender: creator.Gender,
+		Email: creator.Email,
+		Password: creator.Password,
+		Bio: creator.Bio,
+		Category: toCategoryRecords(creator.Category),
+		SocialMedia: toSocialMediaRecords(creator.SocialMedia),
+	}
+}
+//get list
 func (a *Creator) toCore() creator.Core {
 	return creator.Core{
 		ID:		int(a.ID),
--- features/creator/entity.go
+++ features/creator/entity.go
@@ -11,14 +11,32 @@ type Core struct {
 	Name string
 	Location string
 	Age int
-	SocialMedia []SocialMedia
+	Gender string
+	Bio string
+	Email string
+	Password string
+	SocialMedia []SocialMediaCore
+	Category []CategoryCore 
 }
 
-type SocialMedia struct {
+type SocialMediaCore struct {
+	ID int
 	Name string
 	Url string
 	Followers int
 	VerifiedStatus bool
+	CreatorID int
+	RatePrice int
+}
+
+type CategoryCore struct {
+	ID int
+	CategoryName string
+}
+
+type FeaturedContent struct {
+	ID int
+	Url string
 }
 
 //function abstraction
